26ae68e8e96f19d1bb630398402b600b1a8b3403
Hero <gzoo@uzwelo.com>
2023-06-28 22:00:09 -0400

    Init

---
+++ src/main/java/com/th3hero/projectmanagerserver/ProjectManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.th3hero.projectmanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/th3hero/projectmanagerserver/ProjectManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th3hero.projectmanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
