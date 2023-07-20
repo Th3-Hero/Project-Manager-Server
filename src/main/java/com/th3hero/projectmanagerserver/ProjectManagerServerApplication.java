d3cb8f61ed1f91aab44d4fc2f841e0daa3d70b01
Hero <gzoo@uzwelo.com>
2023-07-20 17:18:43 -0400

    Fix

--- src/main/java/com/th3hero/projectmanagerserver/ProjectManagerServerApplication.java
+++ src/main/java/com/th3hero/projectmanagerserver/ProjectManagerServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectManagerServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectManagerServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerServerApplication.class, args);
+    }
 
 }
